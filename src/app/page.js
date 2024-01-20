5a57e5d707dd9f8c524237a3064aadc3432f4454
lam0620 <lam0620@gmail.com>
2024-01-20 13:13:27 +0700

    add amplify

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,119 @@
-import Image from "next/image";
+"use client"
+import Image from 'next/image'
+import { Amplify, Auth } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'
+
+import { getCurrentUser,fetchUserAttributes, fetchAuthSession  } from 'aws-amplify/auth';
+import { get, post } from 'aws-amplify/api';
+import awsconfig from "../aws-exports";
+
+Amplify.configure(awsconfig, {ssr:true}); // <=== Initialize Amplify with the exports config
+const existingConfig = Amplify.getConfig(); // <=== the initialized config should now be returned to existingConfig
+
+Amplify.configure({
+  ...existingConfig,
+  Auth: {
+    Cognito: {
+      userPoolClientId: '3pqkoa0pqj54i1cm8kove9ohir',
+      userPoolId: 'ap-southeast-1_Mx3O0sUk2',
+      // loginWith: { // Optional
+      //   oauth: {
+      //     domain: 'abcdefghij1234567890-29051e27.auth.us-east-1.amazoncognito.com',
+      //     scopes: ['openid email phone profile aws.cognito.signin.user.admin '],
+      //     redirectSignIn: ['http://localhost:3000/','https://example.com/'],
+      //     redirectSignOut: ['http://localhost:3000/','https://example.com/'],
+      //     responseType: 'code',
+      //   },
+      //   username: 'true',
+      //   email: 'false', // Optional
+      //   phone: 'false', // Optional
+      // }
+    },
+    API: {
+      ...existingConfig.API?.REST,
+      REST: {
+        ...existingConfig.API?.REST,
+        'api-sls': {
+          endpoint:
+            'https://kgej3tyhk5.execute-api.ap-southeast-1.amazonaws.com/dev',
+          region: 'ap-southeast-1', // Optional
+
+          // headers: async() => {
+          //   return {Authorization: authToken};
+          // }
+        }
+      }
+    }    
+  }
+});
 
 export default function Home() {
+  const getUserData = async() => {
+    const { username, userId, signInDetails } = await getCurrentUser();
+    const userAttributes = await fetchUserAttributes();
+    const authToken = (await fetchAuthSession()).tokens?.idToken?.toString()
+
+    // console.log(signInDetails);  
+    // console.log(userAttributes);  
+    const restOperation = get({
+      apiName: 'api-sls',
+      path: '/hello',
+      options:{
+        headers:{
+          Authorization: authToken
+        }
+      }
+    });
+    const { body } = await restOperation.response;
+    const str = await body.text();
+
+
+    console.log(str);    
+
+  }
+  const postUserData = async() => {
+    const { username, userId, signInDetails } = await getCurrentUser();
+    const userAttributes = await fetchUserAttributes();
+    const authToken = (await fetchAuthSession()).tokens?.idToken?.toString()
+
+    // console.log(signInDetails);  
+    // console.log(userAttributes);  
+    const restOperation = post({
+      apiName: 'api-sls',
+      path: '/hello',
+      options:{
+        headers:{
+          Authorization: authToken
+        },
+        body:{
+          name: username,
+          email:signInDetails.loginId,
+          age:18
+        }
+      }
+    });
+    const { body, headers } = await restOperation.response;
+    const str = await body.json()
+
+
+    console.log(str);    
+
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    <div>
+      <h1>Serverless tutorial</h1>
+      <Authenticator loginMechanisms={['email']} signUpAttributes={['name']} socialProviders={['google']}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello </h1>
+          <p>Secret message</p>
+          <button onClick={getUserData}>Call GET API</button><br/>
+          <button onClick={postUserData}>Call POST API</button><br/>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+    </div>
+  )
 }
